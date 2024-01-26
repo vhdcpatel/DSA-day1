a548534fbea122306257c314c8c4465fa3a55aa6
vhdcpatel <vrajhpatel21@gmail.com>
2024-01-26 22:18:24 +0530

    Some more code for vector and basic intro of the iterator

--- STL/main.cpp
+++ STL/main.cpp
@@ -8,8 +8,8 @@
 
 // To include all header but this will increase the compilation time of the code.
 // #include <bits/stdc++.h>
-
 using namespace std;
+// Containers for storing data
 
 void learnPair()
 {
@@ -25,10 +25,57 @@ void learnPair()
     cout << arr[2].second;
 }
 
-using namespace std;
+void explainVector()
+{
+    // It has dynamic memory allocation.for array we can't modify the size.
+    vector<int> v1;
+
+    v1.push_back(1);
+    v1.emplace_back(2); // Increase size and insert at the end.
+
+    // pair vector
+    vector<pair<int, int>> vPair;
+
+    vPair.push_back({1, 2});
+    vPair.emplace_back(3, 4);
+
+    cout << vPair[1].second << endl;
+
+    // Giving size of the vector.(size,and element (if only one given then n instance of that one.))
+    vector<int> v(1, 96);
+    v.push_back({10});
+    v.push_back({20});
+    v.push_back({30});
+    v.push_back({40});
+    v.push_back({50});
+
+    // or just size and value will be any garbage value or zero based on the compiler.
+    // (size, value of all)
+    vector<int> v3(10, 96);
+    cout << "**" << v3[0] << endl;
+
+    // Even if we define size we can push_back and emplace_back and increase the size of the vector.
+
+    // to make copy.????
+    vector<int> vCopy(v3);
+    // Both have different memory location.
+    cout << "Memory Locations" << &vCopy << " " << &v3 << endl;
+
+    // Access the element of the vector.
+    cout << v3[0] << v3.at(0);
+
+    // Iterators:
+    vector<int>::iterator it = v.begin();
+    it = it + 3;
+    cout << "Value at the beginnings" << *(it);
+    // Just point to the memory is stored.
+    vector<int>::reverse_iterator it2 = v.rend();   // First of the vector(last of the first )
+    vector<int>::reverse_iterator it3 = v.rbegin(); // last of the vector
+}
 
 int main()
 {
-    learnPair();
+    explainVector();
+    // learnPair();
     return 0;
 }
