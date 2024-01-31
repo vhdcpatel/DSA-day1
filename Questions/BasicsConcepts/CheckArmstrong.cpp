b903be90435918f2008e78ae07c7df4368889da2
vhdcpatel <vrajhpatel21@gmail.com>
2024-01-31 23:32:06 +0530

    Added problems to find armstrong num.

--- Questions/BasicsConcepts/CheckArmstrong.cpp
+++ Questions/BasicsConcepts/CheckArmstrong.cpp
@@ -1,9 +1,34 @@
+#include <math.h>
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
 // #include<bits/stdc++.h>
 using namespace std;
 
-bool checkArmstrong(int n) {}
+int getNumDigits(int n) {
+  int count = 0;
+  while (n) {
+    count++;
+    n = n / 10;
+  }
+  return count;
+}
 
-int main() { return 0; }
+bool checkArmstrong(int n) {
+  int palNo = 0, digit, temp = n, tn;
+  tn = getNumDigits(n);
+  while (temp != 0) {
+    digit = temp % 10;
+    palNo = palNo + pow(digit, tn);
+    temp = temp / 10;
+  }
+  return (palNo == n);
+}
+
+int main() {
+  bool ans;
+  ans = checkArmstrong(1634);
+  cout << ans;
+  return 0;
+}
