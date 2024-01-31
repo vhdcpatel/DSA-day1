1d2017c5b4597491d825e743dc2b382eb6c03dee
vhdcpatel <vrajhpatel21@gmail.com>
2024-01-31 09:35:51 +0530

    Added simple math base problem

---
+++ cppRevice/Questions/oppTrainProblem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void getOppSeatNum(int n) {
+  int mod, pow, oppSeat;
+  mod = n % 12;
+  pow = n / 12;
+  if (mod == 0) {
+    mod = 12;
+    pow--;
+  }
+  oppSeat = 13 - mod;
+  cout << oppSeat + pow * 12;
+  if (oppSeat == 1 || oppSeat == 6 || oppSeat == 7 || oppSeat == 12) {
+    cout << " WS";
+  } else if (oppSeat == 2 || oppSeat == 5 || oppSeat == 8 || oppSeat == 11) {
+    cout << " MS";
+  } else {
+    cout << " AS";
+  }
+}
+
+int main() { getOppSeatNum(40); }
