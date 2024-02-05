0ee58cc917e8be6a2b3ee4d09fc037116e76fd55
Vraj Patel <vrajhpatel21@gmail.com>
2024-02-05 12:19:11 +0000

    Added hashmap based problmes for hashing.

---
+++ Hashing/Questions/HashMaping.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+//#include<bits/stdc++.h>
+using namespace std;
+
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+vector<int> getFrequencies(vector<int> &v) {
+  map<int, int> mpp1;
+  for (int i = 0; i < v.size(); i++) {
+    mpp1[v[i]]++;
+  }
+
+  int maxKey = -1, maxFrequency = -1;
+  int minKey = -1, minFrequency = std::numeric_limits<int>::max();
+
+  for (const auto &pair : mpp1) {
+      if (pair.second > maxFrequency) {
+          maxKey = pair.first;
+          maxFrequency = pair.second;
+      }
+      if (pair.second < minFrequency) {
+          minKey = pair.first;
+          minFrequency = pair.second;
+      }
+  }
+
+  vector<int> ans = {maxKey, minKey};
+  return ans;
+}
+
+int main() {}
+
+int main(){
+ 
+  return 0;
+}
