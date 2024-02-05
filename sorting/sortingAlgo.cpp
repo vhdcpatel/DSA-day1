d7c2d112d8f5f7f41561695b932c9ba97ba44d29
vhdcpatel <vrajhpatel21@gmail.com>
2024-02-05 22:51:41 +0530

    Added code for the Bubble sort.

--- sorting/sortingAlgo.cpp
+++ sorting/sortingAlgo.cpp
@@ -17,6 +17,23 @@ void selectionSort(int n, int arr[]) {
   }
 }
 
+void bubbleSort(int n, int arr[]) {
+  // i>=1
+  for (int i = n - 1; i > 2; i--) {
+    int didSwapHappened = 0;
+    for (int j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr[j], arr[j + 1]);
+        didSwapHappened = 1;
+      }
+      if (!didSwapHappened) {
+        break;
+        // Reduce TC to O(n);
+      }
+    }
+  }
+}
+
 int main() {
   int n;
   cout << "Enter the size of the array";
@@ -25,7 +42,7 @@ int main() {
   for (int i = 0; i < n; i++) {
     cin >> arr[i];
   }
-  selectionSort(n, arr);
+  bubbleSort(n, arr);
 
   // for (int i = 0; i < n; i++) {
   //   cout << arr[i] << " ";
