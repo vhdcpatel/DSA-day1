f635d93209e6698cf99b0aacd9649328f69ab579
vhdcpatel <vrajhpatel21@gmail.com>
2024-02-13 09:09:24 +0530

    Add the xor based solutiin for the missing number problem.

--- Questions/array/EassyProblems/findMissingNumber.cpp
+++ Questions/array/EassyProblems/findMissingNumber.cpp
@@ -4,6 +4,14 @@
 // #include<bits/stdc++.h>
 using namespace std;
 
+int missingNumberBits(vector<int>& nums) {
+  int xor1 = 0;
+  for (int i = 0; i < nums.size(); i++) {
+    xor1 ^= (i + 1) ^ nums[i];
+  }
+  return xor1;
+}
+
 int missingNumber(vector<int>& nums) {
   int n = nums.size(), sumFind;
   sumFind = ((n) * (n + 1)) / 2;
