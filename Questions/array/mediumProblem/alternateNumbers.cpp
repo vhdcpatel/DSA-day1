827c4a9f04dc6b098f8206eb2c9a7c93e24feca2
vhdcpatel <vrajhpatel21@gmail.com>
2024-02-19 22:27:09 +0530

    added one more varient of the problem.

--- Questions/array/mediumProblem/alternateNumbers.cpp
+++ Questions/array/mediumProblem/alternateNumbers.cpp
@@ -17,7 +17,8 @@ vector<int> alternateNumbers(vector<int>& a) {
 
   a.clear();
   int n = min(positive.size(), negative.size());
-  for (int i = 0; i < n; i++) {
+  int i = 0;
+  for (; i < n; i++) {
     a.push_back(positive[i]);
     a.push_back(negative[i]);
   }
@@ -39,6 +40,57 @@ vector<int> rearrangeArray(vector<int>& nums) {
     }
   }
   return ans;
+}
+
+void posAndNeg(vector<int>& arr) {
+  vector<int> positive;
+  vector<int> negative;
+  for (int i = 0; i < arr.size(); i++) {
+    if (arr[i] < 0) {
+      negative.push_back(arr[i]);
+    } else {
+      positive.push_back(arr[i]);
+    }
+  }
+
+  arr.clear();
+  if (positive.size() >= negative.size()) {
+    int i = 0;
+    for (; i < negative.size(); i++) {
+      arr.push_back(positive[i]);
+      arr.push_back(negative[i]);
+    }
+    while (i < positive.size()) {
+      arr.push_back(positive[i++]);
+    }
+  } else {
+    int i = 0;
+    for (; i < positive.size(); i++) {
+      arr.push_back(positive[i]);
+      arr.push_back(negative[i]);
+    }
+    while (i < negative.size()) {
+      arr.push_back(negative[i++]);
+    }
+  }
+
+  // Optimized code.
+  /*
+  // Merge positive and negative numbers alternatively
+    size_t minSize = min(positive.size(), negative.size());
+    for (size_t i = 0; i < minSize; ++i) {
+        arr.push_back(positive[i]);
+        arr.push_back(negative[i]);
+    }
+
+    // Add remaining elements from the larger vector
+    for (size_t i = minSize; i < positive.size(); ++i) {
+        arr.push_back(positive[i]);
+    }
+    for (size_t i = minSize; i < negative.size(); ++i) {
+        arr.push_back(negative[i]);
+    }
+    */
 }
 
 int main() { return 0; }
