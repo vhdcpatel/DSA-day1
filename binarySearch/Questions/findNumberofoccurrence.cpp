cbbe2e91f0d9b9bd26d6f0175cd45087686125a9
Vraj Patel <vrajhpatel21@gmail.com>
2024-02-28 06:42:40 +0000

    Added optimize code.

--- binarySearch/Questions/findNumberofoccurrence.cpp
+++ binarySearch/Questions/findNumberofoccurrence.cpp
@@ -4,6 +4,66 @@
 //#include<bits/stdc++.h>
 using namespace std;
 
+// long but optimal;
+int firstOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int first = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k) {
+            first = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1; // look on the right
+        }
+        else {
+            high = mid - 1; // look on the left
+        }
+    }
+    return first;
+}
+
+int lastOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int last = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k) {
+            last = mid;
+            //look for larger index on the right
+            low = mid + 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1; // look on the right
+        }
+        else {
+            high = mid - 1; // look on the left
+        }
+    }
+    return last;
+}
+
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
+    int first = firstOccurrence(arr, n, k);
+    if (first == -1) return { -1, -1};
+    int last = lastOccurrence(arr, n, k);
+    return {first, last};
+}
+
+int count(vector<int>& arr, int n, int x) {
+    pair<int, int> ans = firstAndLastPosition(arr, n, x);
+    if (ans.first == -1) return 0;
+    return (ans.second - ans.first + 1);
+}
+
+// Mine 
 int count(vector<int>& arr, int n, int x) {
 	int count = 0;
     int low = 0,high = n-1,mid;
@@ -35,6 +95,7 @@ int count(vector<int>& arr, int n, int x) {
     // Better because above will result in the O(n) in worst case 
     // So use binary search to find upper and lower, cut search space for the arr[low] =x,
     // same as the Get floor and Ceil problem. 
+
     // Using STL
     // auto it1=lower_bound(arr.begin(),arr.end(),x);
     // auto it2=upper_bound(arr.begin(),arr.end(),x);
