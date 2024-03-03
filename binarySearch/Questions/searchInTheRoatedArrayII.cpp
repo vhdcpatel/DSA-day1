412d66ea283cb05c194581de0b201af104b68102
vhdcpatel <vrajhpatel21@gmail.com>
2024-03-04 00:29:08 +0530

    Updated and code and Added some more code for better understanding.

--- binarySearch/Questions/searchInTheRoatedArrayII.cpp
+++ binarySearch/Questions/searchInTheRoatedArrayII.cpp
@@ -5,12 +5,22 @@
 using namespace std;
 
 /*
-[2,5,6, 0 ,0,1,2],
+[2,5,6,
+0
+,0,1,2],
 
 
 target = 0
 */
 
+/*
+When there are search and sorted term in the array, always think for the binary
+search.
+- For rotated array focus on finding the sorted half.
+- And one part will be always sorted.
+*/
+
+// Can have duplicate element in the array.
 bool search(vector<int>& nums, int target) {
   int low = 0, high = nums.size() - 1, mid;
 
@@ -18,9 +28,12 @@ bool search(vector<int>& nums, int target) {
     mid = low + (high - low) / 2;
     if (nums[mid] == target) {
       return true;
+      // To handle the edge case:
     } else if (nums[mid] == nums[low] && nums[mid] == nums[high]) {
       low++, high--;
+      // check if we are in the sorted part.
     } else if (nums[mid] < nums[low]) {
+      // Set the low and high for sorted array.
       if (nums[low] <= target && target <= nums[mid]) {
         high = mid - 1;
       } else {
