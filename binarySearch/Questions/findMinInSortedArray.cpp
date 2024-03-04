61471b8bf0e362e0b34997ad542607fd4bf09d5d
Vraj Patel <vrajhpatel21@gmail.com>
2024-03-04 05:43:22 +0000

    Update the solution for finding the duplicate sorted array with variation of binaray search with linear search.

---
+++ binarySearch/Questions/findMinInSortedArray.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+// #include<bits/stdc++.h>
+using namespace std;
+
+int findMin(vector<int>& arr) {
+    
+
+}
+
+int main() { return 0; }
--- binarySearch/Questions/searchInTheRoatedArrayII.cpp
+++ binarySearch/Questions/searchInTheRoatedArrayII.cpp
@@ -18,29 +18,39 @@ When there are search and sorted term in the array, always think for the binary
 search.
 - For rotated array focus on finding the sorted half.
 - And one part will be always sorted.
+- Implemented Linear search for edge case. to handle 
+ex. [0,0,1,0]
 */
 
 // Can have duplicate element in the array.
 bool search(vector<int>& nums, int target) {
-  int low = 0, high = nums.size() - 1, mid;
+  int low = 0, high = nums.size() - 1;
 
   while (low <= high) {
-    mid = low + (high - low) / 2;
-    if (nums[mid] == target) {
-      return true;
-      // To handle the edge case:
-    } else if (nums[mid] == nums[low] && nums[mid] == nums[high]) {
-      low++, high--;
-      // check if we are in the sorted part.
-    } else if (nums[mid] < nums[low]) {
-      // Set the low and high for sorted array.
-      if (nums[low] <= target && target <= nums[mid]) {
+    int mid = low + (high - low) / 2;
+
+    // Check if the middle element is equal to the target
+    if (nums[mid] == target) return true;
+
+    // Handle cases where the low, mid, and high positions have the same value
+    if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+      // Linear search in this scenario
+      for (int i = low; i <= high; ++i) {
+        if (nums[i] == target) return true;
+      }
+      return false;
+    }
+
+    // If left part is sorted
+    if (nums[low] <= nums[mid]) {
+      if (nums[low] <= target && target < nums[mid]) {
         high = mid - 1;
       } else {
         low = mid + 1;
       }
     } else {
-      if (nums[mid] < target && target < nums[high]) {
+      // If right part is sorted
+      if (nums[mid] < target && target <= nums[high]) {
         low = mid + 1;
       } else {
         high = mid - 1;
