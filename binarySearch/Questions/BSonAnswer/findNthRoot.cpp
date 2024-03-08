1c14f241b3bc9dc91ef0e31a15eb14a733f1a6a3
Vraj Patel <vrajhpatel21@gmail.com>
2024-03-08 05:19:12 +0000

    Fix the issues of setting limits in the code.

--- binarySearch/Questions/BSonAnswer/findNthRoot.cpp
+++ binarySearch/Questions/BSonAnswer/findNthRoot.cpp
@@ -4,24 +4,29 @@
 // #include<bits/stdc++.h>
 using namespace std;
 
-int getPowerN(int num, int m) {
-  int ans = 1;
-  for (int i = 1; i <= m; i++) {
+int getPowerNAndCompare(int num, int n, int m) {
+  long long ans = 1;
+  for (int i = 1; i <= n; i++) {
     ans *= num;
+    if (ans > m) {
+      return 0;
+    }
   }
-  return ans;
+  if (ans == m) return 1;
+
+  return 2;
 }
 
 int NthRoot(int n, int m) {
-  int l = 0, h = n;
+  int l = 0, h = m;
   long long mid;
 
   while (l <= h) {
     mid = l + (h - l) / 2;
-    long long power = getPowerN(mid, n);
-    if (power == m) {
+    int ans = getPowerNAndCompare(mid, n, m);
+    if (ans == 1) {
       return mid;
-    } else if (power > m) {
+    } else if (ans == 0) {
       h = mid - 1;
     } else {
       l = mid + 1;
