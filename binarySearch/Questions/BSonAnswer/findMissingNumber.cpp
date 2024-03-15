cd591a7e233ca4dcd06b4123bbf9f554169c7430
vhdcpatel <vrajhpatel21@gmail.com>
2024-03-15 15:33:17 +0530

    Added Find K the missing number problem.

--- binarySearch/Questions/BSonAnswer/findMissingNumber.cpp
+++ binarySearch/Questions/BSonAnswer/findMissingNumber.cpp
@@ -18,8 +18,18 @@ int missingKBrute(vector<int> vec, int n, int k) {
 };
 
 // optimized
-int missingK(vector<int> vec, int n, int k){
-
+int missingK(vector<int> vec, int n, int k) {
+  int low = 0, high = n - 1, mid;
+  while (low <= high) {
+    mid = low + (high - low) / 2;
+    int missingNumbersCurrent = vec[mid] - (mid + 1);
+    if (missingNumbersCurrent < k) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return high + k + 1;
 };
 
 int main() { return 0; }
